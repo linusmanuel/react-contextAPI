4d4af74b03b380a30d7e0514898d765e1913e5bb
Joao Lino <linusmannuel@gmail.com>
2023-01-24 20:25:10 +0100

    feat: Add routes and props

--- src/pages/Feira/index.js
+++ src/pages/Feira/index.js
@@ -1,37 +1,27 @@
-import {
-  Container,
-  Header,
-  Lista,
-} from './styles';
+import { Container, Header, Lista } from './styles';
 import feira from './feira.json';
 import Produto from 'components/Produto';
 import NavBar from './NavBar';
 
-
-function Feira() {
-  return (
-    <Container>
-      <NavBar />
-      <Header>
-        <div>
-          <h2> Olá!</h2>
-          <h3> Saldo: R$</h3>
-        </div>
-        <p>Encontre os melhores produtos orgânicos!</p>
-      </Header>
-      <Lista>
-        <h2>
-          Produtos:
-        </h2>
-        {feira.map(produto => (
-          <Produto
-            {...produto}
-            key={produto.id}
-          />
-        ))}
-      </Lista>
-    </Container>
-  )
+function Feira({ nome, saldo }) {
+	return (
+		<Container>
+			<NavBar />
+			<Header>
+				<div>
+					<h2> Olá! {nome}</h2>
+					<h3> Saldo: {saldo} R$</h3>
+				</div>
+				<p>Encontre os melhores produtos orgânicos!</p>
+			</Header>
+			<Lista>
+				<h2>Produtos:</h2>
+				{feira.map((produto) => (
+					<Produto {...produto} key={produto.id} />
+				))}
+			</Lista>
+		</Container>
+	);
 }
 
 export default Feira;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,50 +1,39 @@
 import { Button } from '@material-ui/core';
-import {
-  Container,
-  Titulo,
-  InputContainer
-} from './styles';
-import {
-  Input,
-  InputLabel,
-  InputAdornment 
-} from '@material-ui/core';
+import { Container, Titulo, InputContainer } from './styles';
+import { Input, InputLabel, InputAdornment } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-function Login() {
-  return (
-    <Container>
-      <Titulo>
-        Insira o seu nome
-      </Titulo>
-      <InputContainer>
-        <InputLabel>
-          Nome
-        </InputLabel>
-        <Input
-          type="text"
-        />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>
-          Saldo
-        </InputLabel>
-        <Input
-        type="number"
-        startAdornment={
-          <InputAdornment position="start">
-            R$
-          </InputAdornment>
-        }
-      />
-      </InputContainer>
-      <Button
-        variant="contained"
-        color="primary"
-      >
-        Avançar
-      </Button>
-    </Container>
-  )
-};
+function Login({ nome, setNome, saldo, setSaldo }) {
+	const history = useHistory();
+	return (
+		<Container>
+			<Titulo>Insira o seu nome</Titulo>
+			<InputContainer>
+				<InputLabel>Nome</InputLabel>
+				<Input
+					value={nome}
+					onChange={(event) => setNome(event.target.value)}
+					type="text"
+				/>
+			</InputContainer>
+			<InputContainer>
+				<InputLabel>Saldo</InputLabel>
+				<Input
+					type="number"
+					value={saldo}
+					onChange={(event) => setSaldo(event.target.value)}
+					startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+				/>
+			</InputContainer>
+			<Button
+				variant="contained"
+				color="primary"
+				onClick={() => history.push('/feira')}
+			>
+				Avançar
+			</Button>
+		</Container>
+	);
+}
 
 export default Login;
