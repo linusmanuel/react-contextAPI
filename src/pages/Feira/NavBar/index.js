88206fd816394594c19096f6f2e4c0f87dbc8e86
Joao Lino <linusmannuel@gmail.com>
2023-02-07 21:29:31 +0100

    feat: Add carrinho provider on new route

--- src/pages/Feira/NavBar/index.js
+++ src/pages/Feira/NavBar/index.js
@@ -4,13 +4,18 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
 import { useCarrinhoContext } from 'common/context/Carrinho';
+import { useHistory } from 'react-router-dom';
 
 export default function NavBar() {
 	const { quantidadeProduto } = useCarrinhoContext();
+	const history = useHistory();
 	return (
 		<Nav>
 			<Logo />
-			<IconButton disabled={quantidadeProduto === 0}>
+			<IconButton
+				disabled={quantidadeProduto === 0}
+				onClick={() => history.push('/carrinho')}
+			>
 				<Badge
 					overlap="rectangular"
 					color="primary"
